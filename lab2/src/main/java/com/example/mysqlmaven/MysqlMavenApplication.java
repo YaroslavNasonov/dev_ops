37ede33cf00d0ab3161f0092252e9ddbe8141729
YaroslavNasonov <90750068+YaroslavNasonov@users.noreply.github.com>
2024-03-18 15:54:18 +0300

    Add files via upload

---
+++ lab2/src/main/java/com/example/mysqlmaven/MysqlMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlMavenApplication.class, args);
+	}
+
+}
